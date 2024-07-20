2b08ac837ff54b29d69ad02b48518aeaea6e382f
Mohammed Faizan <127293787+xmfaizan@users.noreply.github.com>
2024-07-21 00:38:44 +0530

    added 1281

---
+++ 1281-SubProdandSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n){
+                int sum = 0, prod = 1;
+        
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit;
+            prod *= digit;
+            n /= 10;
+        }
+        
+        int ans = prod - sum;   
+        return ans; 
+    }
+};
