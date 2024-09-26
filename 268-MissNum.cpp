024593f74bc9eaaa09df3c943684aa9290b42b45
Mohammed Faizan <127293787+xmfaizan@users.noreply.github.com>
2024-09-26 23:44:03 +0530

    added 268

---
+++ 268-MissNum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+            int xor1 = 0, xor2 = 0;
+            int n = nums.size();
+
+            for (int i = 0; i < n ; i++) {
+                xor2 = xor2 ^ nums[i]; 
+                xor1 = xor1 ^ i; 
+            }
+        xor1 = xor1 ^ n;
+
+        return (xor1 ^ xor2);
+        
+    }
+};
