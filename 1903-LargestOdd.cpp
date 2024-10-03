88c4194dbd09850bd8299f16ea3e22c2f08c10a4
Mohammed Faizan <127293787+xmfaizan@users.noreply.github.com>
2024-10-03 23:56:55 +0530

    added 1903

---
+++ 1903-LargestOdd.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        
+        for (int i = num.size() - 1; i >= 0; --i) {
+            // Check if the digit is odd
+            if ((num[i] - '0') % 2 == 1) {
+                
+                return num.substr(0, i + 1);
+            }
+        }
+       
+        return "";
+    }
+};
