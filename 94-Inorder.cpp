3ae211a19110ce76bdce9d1f7b8411365aa6a83f
Mohammed Faizan <127293787+xmfaizan@users.noreply.github.com>
2024-10-08 18:19:39 +0530

    added 94

---
+++ 94-Inorder.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void inorder(TreeNode* root, vector<int> &arr){
+    // If the current node is NULL
+    // (base case for recursion), return
+    if(root == nullptr){
+        return;
+    }
+    // Push the current node's
+    // value into the vector
+    inorder(root->left, arr);
+
+    arr.push_back(root->val);
+
+    inorder(root->right, arr);
+}
+
+    vector<int> inorderTraversal(TreeNode* root) {
+    // Create an empty vector to
+    // store inorder traversal values
+    vector<int> arr;
+    // Call the inorder traversal function
+    inorder(root, arr);
+    // Return the resulting vector
+    // containing inorder traversal values
+    return arr;
+}
+};
