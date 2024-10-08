e7f19b1124ffe87ae38906a9ebf91913e2459b33
Mohammed Faizan <127293787+xmfaizan@users.noreply.github.com>
2024-10-08 18:21:49 +0530

    added 145

---
+++ 145-PostOrder.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void postorder(TreeNode* root, vector<int> &arr){
+    // If the current node is NULL
+    // (base case for recursion), return
+    if(root == nullptr){
+        return;
+    }
+    // Push the current node's
+    // value into the vector
+    postorder(root->left, arr);
+
+    postorder(root->right, arr);
+
+    arr.push_back(root->val);
+
+}
+
+    vector<int> postorderTraversal(TreeNode* root) {
+    // Create an empty vector to
+    // store postorder traversal values
+    vector<int> arr;
+    // Call the postorder traversal function
+    postorder(root, arr);
+    // Return the resulting vector
+    // containing postorder traversal values
+    return arr;
+}
+};
