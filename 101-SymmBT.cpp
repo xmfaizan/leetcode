1f8d6098e0d4ab1c176d2b3de8dacec97d76ebf3
Mohammed Faizan <127293787+xmfaizan@users.noreply.github.com>
2024-10-09 21:32:30 +0530

    added 101

---
+++ 101-SymmBT.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetricUtil(TreeNode* root1, TreeNode* root2) {
+        // Check if either subtree is NULL
+        if (root1 == NULL || root2 == NULL) {
+            // If one subtree is NULL, the other
+            // must also be NULL for symmetry
+            return root1 == root2;
+        }
+
+        return (root1->val == root2->val)
+            && isSymmetricUtil(root1->left, root2->right)
+            && isSymmetricUtil(root1->right, root2->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        // Check if the tree is empty
+        if (!root) {
+
+            return true;
+        }
+        return isSymmetricUtil(root->left, root->right);
+    }
+};
