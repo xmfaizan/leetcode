83327d26ea05da592a7a71618d6781fb82ab429b
Mohammed Faizan <127293787+xmfaizan@users.noreply.github.com>
2024-10-11 12:49:47 +0530

    added 222

---
+++ 222-NodesinBT.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root == NULL) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int count = 0;
+        while (!q.empty()) {
+            TreeNode* current = q.front();
+            q.pop();
+            count++;
+            if (current->left != NULL) {
+                q.push(current->left);
+            }
+            if (current->right != NULL) {
+                q.push(current->right);
+            }
+        }
+        return count;
+    }
+};
