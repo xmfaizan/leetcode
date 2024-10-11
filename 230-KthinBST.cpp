320d756d09d53bfe1837667aaa84df8e70ca4dab
Mohammed Faizan <127293787+xmfaizan@users.noreply.github.com>
2024-10-11 15:52:59 +0530

    added 230

---
+++ 230-KthinBST.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, int k, int &ans, int &cnt){
+        if(root == nullptr || cnt >= k) return;
+
+        inorder(root->left, k, ans, cnt);
+
+        cnt++;
+        if(cnt == k){
+            ans = root->val;
+            return;
+        }
+
+        inorder(root->right, k, ans, cnt);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        int ans = INT_MAX;
+        int cnt = 0;
+
+        inorder(root, k, ans, cnt);
+
+        return ans;
+    }
+};
